65a027a97517bb4e5e0c57e0663b617d97660977
Tapolcsanyi <iceike237@gmail.com>
2022-02-03 16:05:52 -0600

    this is getting quite frusterating

--- clientrealthistime/src/components/myCollection/myCollection.js
+++ clientrealthistime/src/components/myCollection/myCollection.js
@@ -15,7 +15,7 @@ export const UserSneakerList = () => {
         getSneakers();
     }, []);
 
-    let pp = sneakers.filter( (ele, ind) => ind === sneakers.findIndex( elem => elem.SneakerId === ele.jobid && elem.id === ele.id))
+    let pp = sneakers.filter((ele, ind) => ind === sneakers.findIndex( elem => elem.SneakerId === ele.SneakerId && elem.id == ele.id))
 
     console.log(pp)
 
